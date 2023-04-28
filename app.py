0eba54449f8dce87373de45f9d3fa2c768020442
vburdyk <burdikv1991@gmail.com>
2023-04-28 14:18:42 +0300

    Added docker files

--- app.py
+++ app.py
@@ -5,10 +5,22 @@ from flask_login import LoginManager
 
 
 app = Flask(__name__)
-db = SQLAlchemy()
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:10203040@localhost:3306/flask"
-app.secret_key = "sadasdsdssadsadsadsadsadssaddas"
-db.init_app(app)
+
+db = {
+    'mysql_user': 'flask',
+    'mysql_password': 'flask',
+    'mysql_host': 'db:3306',
+    'mysql_db': 'flask'
+}
+
+connection_str='mysql+pymysql://'+db['mysql_user']+':'+db['mysql_password']+'@'+db['mysql_host']+'/'+db['mysql_db']
+app.config['SQLALCHEMY_DATABASE_URI']=connection_str
+
+db = SQLAlchemy(app)
+
+# app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://flask:flask@db:3306/flask"
+# app.secret_key = "sadasdsdssadsadsadsadsadssaddas"
+# db.init_app(app)
 
 manager = LoginManager(app)
 #https://www.youtube.com/watch?v=XUwOOqRDAHE&list=PLU2ftbIeotGrVZA85M6PnXSOHY7MnTBCc&index=4&ab_channel=letsCode
